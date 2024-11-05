3062e01ebac45019e34cb121d545af8a6ccc6de5
Clavito <pablo.alvarado.umana@est.una.ac.cr>
2024-11-05 00:29:43 -0600

    Cambio del title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider, Helmet } from 'react-helmet-async'; // Importa Helmet y HelmetProvider
 import Layout from './layout/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';  // Aquí importas Tailwind CSS
+import './index.css'; // Aquí importas Tailwind CSS
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <HelmetProvider> {/* Envuelve tu aplicación con HelmetProvider */}
+      <Helmet>
+        <title>Gestor BD</title> {/* Establece el título aquí */}
+        <meta name="description" content="Gestor de base de datos para administrar tus datos." />
+      </Helmet>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
